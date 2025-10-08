4fc883c9029abd1d37dbf4b24f0250fb06a0bca7
Oleh Bozhak <olbozhak@gmail.com>
2025-10-08 16:32:34 +0300

    Perfect

--- app/main.py
+++ app/main.py
@@ -1,1 +1,39 @@
-# write your code here
+class AliveList(list):
+    def __str__(self):
+        return "[" + ", ".join(
+            f"{{Name: {i.name}, Health: {i.health}, Hidden: {i.hidden}}}" for i in self
+        if i.health > 0) + "]"
+
+class Animal:
+    alive = AliveList()
+
+    def __init__(self, name, health: int = 100, hidden: bool = False):
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+
+class Herbivore(Animal):
+    def hide(self):
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, other):
+        if not isinstance(other, Carnivore) and not other.hidden and other.health > 0:
+            other.health -= 50
+
+
+lion = Carnivore("Lion")
+Parrot = Herbivore("Parrot")
+
+
+lion.health = 0
+#
+# Parrot.hide()
+# Parrot.hide()
+#
+# lion.bite(Parrot)
+# lion.bite(Parrot)
+print(Animal.alive)
