9dc6d0fb3be12a33f522d4275058c73408d010aa
Oleh Bozhak <olbozhak@gmail.com>
2025-10-08 16:34:04 +0300

    Previous example

--- app/main.py
+++ app/main.py
@@ -2,16 +2,27 @@ class AliveList(list):
     def __str__(self):
         return "[" + ", ".join(
             f"{{Name: {i.name}, Health: {i.health}, Hidden: {i.hidden}}}" for i in self
-        if i.health > 0) + "]"
+        ) + "]"
+
 
 class Animal:
     alive = AliveList()
 
     def __init__(self, name, health: int = 100, hidden: bool = False):
         self.name = name
-        self.health = health
+        self._health = health
         self.hidden = hidden
         Animal.alive.append(self)
+
+    @property
+    def health(self):
+        return self._health
+
+    @health.setter
+    def health(self, value):
+        self._health = value
+        if self._health <= 0 and self in Animal.alive:
+            Animal.alive.remove(self)
 
 
 class Herbivore(Animal):
@@ -30,10 +41,10 @@ Parrot = Herbivore("Parrot")
 
 
 lion.health = 0
-#
-# Parrot.hide()
-# Parrot.hide()
-#
-# lion.bite(Parrot)
-# lion.bite(Parrot)
+
+Parrot.hide()
+Parrot.hide()
+
+lion.bite(Parrot)
+lion.bite(Parrot)
 print(Animal.alive)
